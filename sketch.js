a8961ae583b46e04cc49683c20042319133e718b
Shakti <65832818+Shaktinetra@users.noreply.github.com>
2020-07-24 22:10:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -74,7 +74,6 @@ function draw() {
     
         } else {
           if (particle.body.position.x > 601 && particle.body.position < 800) {
-            score = score + 200;
             particle = null;
           }
         }
